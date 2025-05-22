9745d729470775647fe811326d574384447469c0
bbek4210 <bhandaribibek731@gmail.com>
2025-05-23 00:28:01 +0545

    fix

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -6,3 +6,6 @@ export const db = mysql.createPool({
   password: "Qwerty123$",
   database: "rent_a_ride",
 });
+db.getConnection()
+  .then(() => console.log("✅ MySQL connected"))
+  .catch((err) => console.error("❌ MySQL connect error", err));
